e77dca606217e36ee0391d7288e75b85c7ae1793
rtakatori49@gmail.com <rtakatori49@gmail.com>
2020-03-31 18:24:13 -0700

    Updated Code

--- Automated Fishbowl/Automated_Fishbowl.py
+++ Automated Fishbowl/Automated_Fishbowl.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from datetime import datetime, timedelta
 
-chromedriver = 'C:\\Users\\rtold\\Downloads\\chromedriver'
+chromedriver = 'D:\\Downloads\\chromedriver'
 browser = webdriver.Chrome(chromedriver)
 browser.get('http://schedule.lib.calpoly.edu/rooms.php?i=2015')
 
@@ -12,8 +12,10 @@ delta_date = timedelta(14)
 target_date = current_date + delta_date
 target_month = target_date.strftime("%m")
 target_day = target_date.strftime("%d")
-if target_month != current_month:
+if target_month != current_month and target_day != "14":
     next_button = browser.find_element_by_xpath('//*[@id="s-lc-rm-cal"]/div/div/a[2]/span')
     next_button.click()
 calendar_day = browser.find_element_by_link_text(target_day)
 calendar_day.click()
+reserve_time_1 = browser.find_element_by_xpath('//a[@data-seq = "5414101"]')
+reserve_time_1.click()
