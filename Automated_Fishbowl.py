9227a6747b2313323ef66fd88df9b757d28d9e05
Ryo Takatori <rtakatori49@gmail.com>
2021-11-07 01:59:53 -0800

    Cleaned up create_user_file.py and removed user related things off of automated_fishbowl.py. Added auto download for chromedriver. Updated time elements and adjusted times.

--- Automated_Fishbowl.py
+++ Automated_Fishbowl.py
@@ -4,18 +4,12 @@
 # Created to automate fishbowl reservation
 
 # Modules
+import chromedriver_autoinstaller
 import datetime
 import json
-import logging
-import os
 import time
-import sys
 
 from selenium import webdriver
-from datetime import datetime, timedelta
-from tkinter import *
-from tkinter import messagebox
-from PIL import Image, ImageTk
 
 # Load json
 def load_json(filename):
@@ -23,140 +17,32 @@ def load_json(filename):
         data = json.load(f)
     return data
 
+# Load time element specific to user choice
+def get_time_element():
+    user_info = load_json("user.json")
+    reserve_time = load_json("reserve_time.json")
+    time_element = load_json("time_element.json")
+    reserve_time_list = reserve_time[user_info["selected_time"]]
+    time_element_list = [time_element[user_info["room_name"]][x] for x in reserve_time_list]
+    return time_element_list
 
-# Classroom class
-class FishbowlRoom:
-    def __init__(self, time_dict):
-        pass
-
-# User class
-class Reserver:
-    def __init__(self):
-        self.username = ""        
-        
-
-
-# Create grid
-class GridCreator:
-    def __init__(self, root, name, row, column, columnspan):
-        self.root = root
-        self.name = name
-        self.row = row
-        self.column = column
-        self.columnspan = columnspan
-    def create_grid(self):
-            Label(self.root, text=self.name).grid(row=self.row, column=self.column, columnspan=self.columnspan)
-
-# Create entry
-class EntryCreator(GridCreator):
-    def __init__(self, root, name, row, column, columnspan, column_entry, width):
-        super().__init__(root, name, row, column, columnspan)
-        self.column_entry = column_entry
-        self.width = width
-    def create_entry(self):
-        super().create_grid()
-        entry = Entry(self.root, width=self.width)
-        entry.grid(row=self.row, column=self.column_entry)
-        return entry
-
-# Create dropdown
-class DropdownCreator(GridCreator):
-    def __init__(self, root, name, row, column, columnspan, column_entry, variable_list):
-        super().__init__(root, name, row, column, columnspan)
-        self.column_entry = column_entry
-        self.variable_list = variable_list
-    def create_dropdown(self):
-        super().create_grid()
-        variable = StringVar(self.root)
-        variable.set(self.variable_list[0])
-        OptionMenu(self.root, variable, *self.variable_list).grid(row=self.row, column=self.column_entry)
-        return variable
-
-def whatever():
-    # User interface for initial information
-    room_number_list = ["216L",
-                        "216N",
-                        "216Q",
-                        "216S"
-    ]
-    time_list = ["8:00 AM ~ 10:00 AM",
-                "11:00 AM ~ 1:00 PM",
-                "2:00 PM ~ 4:00 PM",
-                "5:00 PM ~ 7:00 PM",
-                "8:00 PM ~ 10:00 PM",
-    ]
-    file_exists = os.path.isfile("userinfo.txt")
-    if not file_exists:
-        # Page settings
-        root = Tk()
-        root.wm_iconbitmap('fish.ico') # Icon
-        root.geometry("450x250") # Size
-        root.title("Automated Fishbowl Reserver") # Title
-        grid_title = GridCreator(root,"Please enter your information:",0,0,2).create_grid() # Grid title
-        first_name = EntryCreator(root,"First Name:",1,0,1,1,20).create_entry() # First name
-        last_name = EntryCreator(root,"Last Name:",2,0,1,1,20).create_entry() # Last name
-        email = EntryCreator(root,"Cal Poly Email:",3,0,1,1,20).create_entry() # Email
-        group_name = EntryCreator(root,"Group Name:",4,0,1,1,20).create_entry() # Group name
-        room_number = DropdownCreator(root,"Room Number:",5,0,1,1,room_number_list).create_dropdown() # Room number
-        time = DropdownCreator(root,"Reservation Time:",6,0,1,1,time_list).create_dropdown() # Reservation time
-
-        # Image
-        image = Image.open("fishbowl.gif")
-        photo = ImageTk.PhotoImage(image)
-        photo_label = Label(image=photo)
-        photo_label.image = photo # this line need to prevent gc
-        photo_label.grid(row=0, column=2, columnspan=7, rowspan=7)
-
-        # Submission
-        def my_click():
-            my_first_name = first_name.get()
-            my_last_name = last_name.get()
-            my_email = email.get()
-            my_group_name = group_name.get()
-            my_room_number = room_number.get()
-            my_time = time.get()
-            user_info = open("userinfo.txt", "w")
-            user_info.writelines('\n'.join([my_first_name, my_last_name, my_email, my_group_name, my_room_number, my_time]))
-            user_info.close()
-            root.quit()
-        my_button = Button(root, text="Submit", command=my_click)
-        my_button.grid(row=7, column=1)
-        root.mainloop()
-        messagebox.showinfo('Completed!', 'Initial information applied. Please configure Task Scheduler to automate reservation.')
-        sys.exit(0)
-
-    # Save user information in text file
-    user_info = open("userinfo.txt", "r")
-    user_info_list = user_info.read().splitlines()
-    user_info.close()
-
-    # Assign correct room number and time
-    time_element_data_locator = time_list.index(user_info_list[5])
-    time_element_list = []
-    if user_info_list[4] == "216L":
-        time_element_data = open("216L.txt","r")
-    elif user_info_list[4] == "216N":
-        time_element_data = open("216N.txt","r")
-    elif user_info_list[4] == "216Q":
-        time_element_data = open("216Q.txt","r")
-    else:
-        time_element_data = open("216S.txt","r")
-    for c, value in enumerate(time_element_data.read().splitlines()):
-            if c >= time_element_data_locator*3 and c <= time_element_data_locator*3+2:
-                time_element_list.append(value)
-    time_element_data.close()
+# Reservation
+def reserve():
+    # Load user preference
+    user_info = load_json("user.json")
+    time_element_list = get_time_element()
 
     ## Open browser
+    chromedriver_autoinstaller.install()
     browser = webdriver.Chrome()
     browser.get('http://schedule.lib.calpoly.edu/rooms.php?i=2015')
 
     # Today's date
-    current_date = datetime.now()
+    current_date = datetime.datetime.now()
     current_month = current_date.strftime("%m")
-    current_day = current_date.strftime("%d")
 
     # Target date (2 week ahead)
-    delta_date = timedelta(14)
+    delta_date = datetime.timedelta(14)
     target_date = current_date + delta_date
     target_month = target_date.strftime("%m")
     target_day = target_date.strftime("%d")
@@ -172,31 +58,26 @@ def whatever():
     time.sleep(1)
 
     # Reserve 3 hours
-    reserve_time_1 = browser.find_element_by_xpath('//*[@data-seq="{}"]'.format(time_element_list[0]))
-    reserve_time_1.click()
-    reserve_time_2 = browser.find_element_by_xpath('//*[@data-seq="{}"]'.format(time_element_list[1]))
-    reserve_time_2.click()
-    reserve_time_3 = browser.find_element_by_xpath('//*[@data-seq="{}"]'.format(time_element_list[2]))
-    reserve_time_3.click()
+    for time_element in time_element_list:
+        reserve_time = browser.find_element_by_xpath(f"//*[@data-seq={time_element}]")
+        reserve_time.click()
 
     # Fill in form
     first_name = browser.find_element_by_xpath('//*[@id="fname"]')
-    first_name.send_keys(user_info_list[0])
+    first_name.send_keys(user_info["first_name"])
     last_name = browser.find_element_by_xpath('//*[@id="lname"]')
-    last_name.send_keys(user_info_list[1])
+    last_name.send_keys(user_info["last_name"])
     email = browser.find_element_by_xpath('//*[@id="email"]')
-    email.send_keys(user_info_list[2])
+    email.send_keys(user_info["email"])
     group_name = browser.find_element_by_xpath('//*[@id="nick"]')
-    group_name.send_keys(user_info_list[3])
+    group_name.send_keys(user_info["group_name"])
 
     # Submit
     submit = browser.find_element_by_xpath('//*[@id="s-lc-rm-sub"]')
     submit.click()
 
 def main():
-    data = load_json("time_element.json")
-    print(data)
-    whatever()
+    reserve()
 
 if __name__ == "__main__":
     main()
--- create_user_file.py
+++ create_user_file.py
@@ -1,7 +1,14 @@
+# Create User File for Automated Fishbowl Reservation
+# Ryo Takatori
+# 2021-11-07
+
+# Modules
 import json
 import os
+import sys
 import tkinter
 
+from tkinter import messagebox
 from PIL import Image, ImageTk
 
 # Load json
@@ -10,101 +17,111 @@ def load_json(filename):
         data = json.load(f)
     return data
 
-def create_user():
-    user_dict = {
-        "first_name": "",
-        "last_name": "",
-        "email": "",
-        "group_name": "",
-        "room_name": "",
-        "selected_time": ""
-    }
+# Dump json
+def dump_json(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=6)
 
-
+# UI class
 class UI:
     def __init__(self):
         self.icon_windows = "resources/fish.ico"
+        self.icon_linux = "resources/fish.xbm"
+        self.image = "resources/fishbowl.gif"
         self.geometry = "450x250"
         self.title = "Create User Profile"
-        self.grids = {
-            "page_name": "Please enter your information:",
-            "first_name": "First Name:",
-            "last_name": "Last Name:",
-            "email": "Cal Poly Email:",
-            "group_name": "Group Name:",
-            "room_number": "Room Number:",
-            "time": "Reservation Time:"
-        }
+        self.grids = [
+            "Please enter your information:",
+            "First Name:",
+            "Last Name:",
+            "Cal Poly Email:",
+            "Group Name:",
+            "Room Number:",
+            "Reservation Time:"
+        ]
         self.entries = [
             "First Name:",
             "Last Name:",
             "Cal Poly Email:",
             "Group Name:"
         ]
-        self.dropdowns = [
-            "Room Number:",
-            "Reservation Time:"
-        ]
-        self.time_list = [
-            "8:00 AM ~ 10:00 AM",
-            "11:00 AM ~ 1:00 PM",
-            "2:00 PM ~ 4:00 PM",
-            "5:00 PM ~ 7:00 PM",
-            "8:00 PM ~ 10:00 PM"
-        ]
+        self.dropdowns = {
+            "Room Number:": [x for x in load_json("time_element.json")],
+            "Reservation Time:": [x for x in load_json("reserve_time.json")]
+        }
 
+# Create grid
 def create_grid(root, name, row, column=0, columnspan=1):
     grid_label = tkinter.Label(root, text=name)
     grid_label.grid(row=row,
                     column=column,
                     columnspan=columnspan)
 
+# Create entry
 def create_entry(root, row, column_entry=1, width=20):
     entry = tkinter.Entry(root, width=width)
     entry.grid(row=row, column=column_entry)
     return entry
 
-def create_dropdown(root, variable_list, row, column_entry):
+# Create dropdown
+def create_dropdown(root, variable_list, row, column_entry=1):
     variable = tkinter.StringVar(root)
     variable.set(variable_list[0])
     option_menu = tkinter.OptionMenu(root, variable, *variable_list)
     option_menu.grid(row=row, column=column_entry)
     return variable
 
-
-def create_ui(ui):
-    # Create UI
+# Create UI
+def create_ui():
+    ui = UI()
     root = tkinter.Tk()
     root.wm_iconbitmap(ui.icon_windows) # icon
     root.geometry(ui.geometry) # page size
     root.title(ui.title) # title
+    entry_list = []
     for idx, name in enumerate(ui.grids):
         if idx == 0:
-            create_grid(root, ui.grids[name], idx, columnspan=2)
+            create_grid(root, name, idx, columnspan=2)
         else:
-            create_grid(root, ui.grids[name], idx)
-<<<<<<< HEAD
-        if ui.grids[name] in ui.entries:
-=======
+            create_grid(root, name, idx)
         if name in ui.entries:
->>>>>>> febfae0c0b126bf6cafbf87b64d563ed8576ab96
-            create_entry(root, idx)
-        if ui.grids[name] in ui.dropdowns:
-            create_dropdown(root, idx)
+            entry_list.append(create_entry(root, idx))
+        if name in ui.dropdowns:
+            entry_list.append(create_dropdown(root, ui.dropdowns[name], idx))
+    # Image
+    image = Image.open(ui.image)
+    photo = ImageTk.PhotoImage(image)
+    photo_label = tkinter.Label(image=photo)
+    photo_label.image = photo # this line need to prevent gc
+    photo_label.grid(row=0, column=2, columnspan=7, rowspan=7)
+    # Submission
+    def my_click():
+        user_dict = {
+            "first_name": entry_list[0].get(),
+            "last_name": entry_list[1].get(),
+            "email": entry_list[2].get(),
+            "group_name": entry_list[3].get(),
+            "room_name": entry_list[4].get(),
+            "selected_time": entry_list[5].get()
+        }
+        dump_json(user_dict, "user.json")
+        root.quit()
+    my_button = tkinter.Button(root, text="Submit", command=my_click)
+    my_button.grid(row=7, column=1)
     root.mainloop()
-    user_dict = {
-        "first_name": "",
-        "last_name": "",
-        "email": "",
-        "group_name": "",
-        "room_name": "",
-        "selected_time": ""
-    }
-    return user_dict
+    messagebox.showinfo("Completed!",
+        "Initial information applied. "\
+        "Please configure Task Scheduler to automate reservation.")
+    sys.exit(0)
 
 def main():
-    ui = UI()
-    create_ui(ui)
+    if os.path.isfile("user.json"):
+        messagebox.showinfo("Completed!",
+            "User already configured. "\
+            "Please configure Task Scheduler to automate reservation.")
+        sys.exit(0)
+    else:
+        create_ui()
 
 if __name__ == "__main__":
     main()
